4fbb2f10f4c86f030f80f6fe06eb817f174ea7e9
Noor <42749075+LightDvlpr@users.noreply.github.com>
2020-07-18 00:03:15 -0400

    Update MenuImplementation.java

--- src/MenuImplementation.java
+++ src/MenuImplementation.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class MenuImplementation implements Menu {
 
-    private static String FILENAME = "products.txt";
+    private static String FILENAME = "Products.txt";
     private static final String DELIMITER = ",";
     private List<Item> menu = new ArrayList<Item>();
     private List<Item> currentMenu = new ArrayList<>();
