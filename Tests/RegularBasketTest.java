b1c6de84efbad2ac9a905adea3eb34c1ff3c2bb6
Nsalim <lightsaud@gmail.com>
2020-07-19 15:20:44 -0400

    No longer busy with work so I can add tests now. Hope that's alright!

---
+++ Tests/RegularBasketTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Test;
+
+class RegularBasketTest {
+
+    @Test
+    void returnAllBasketItems() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void adjustBasketState() {
+    }
+
+    @Test
+    void addItemToBasket() {
+    }
+
+    @Test
+    void getItemFromBasket() {
+    }
+
+    @Test
+    void salesTax() {
+    }
+
+    @Test
+    void imprtTaxtotal() {
+    }
+
+    @Test
+    void calculate() {
+    }
+
+    @Test
+    void taxTotal() {
+    }
+
+    @Test
+    void nontaxTotal() {
+    }
+}
---
+++ Tests/RegularMenuTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+class RegularMenuTest {
+
+    @Test
+    void getItemFromMenu() {
+    }
+
+    @Test
+    void returnAllMenuItems() {
+    }
+
+    @Test
+    void readFile() {
+    }
+
+    @Test
+    void writeToFile() {
+    }
+
+    @Test
+    void getLastItem() {
+    }
+
+    @Test
+    void addItemToMenu() {
+    }
+}
